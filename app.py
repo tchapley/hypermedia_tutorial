ef7d04d79daf7d10df36cf9dc2d0d4440a1dfcea
TJ Chapley <tj@chapley.com>
2025-02-16 09:41:09 -0500

    Initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask, redirect, render_template, request
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return redirect('/contacts')
+
+@app.route('/contacts')
+def contacts():
+    search = request.args.get('q')
+    if search is not None:
+        contacts_set = Contact.search(search)
+    else:
+        contacts_set = Contact.all()
+
+    return render_template('index.html', contacts=contacts_set)
