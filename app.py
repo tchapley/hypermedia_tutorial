52c632f05d4400b74d37eb639832f2a5c6347a08
TJ Chapley <tj@chapley.com>
2025-02-16 11:23:07 -0500

    Added HTML templates Added functionality for viewing blog posts and adding blog posts

--- app.py
+++ app.py
@@ -1,17 +1,43 @@
-from flask import Flask, redirect, render_template, request
+import datetime
+from flask import (
+    Flask, redirect, render_template, request, flash
+)
+from posts_model import Post
+
+Post.load_db()
 
 app = Flask(__name__)
 
+app.secret_key = b'hypermedia rocks'
+
 @app.route('/')
 def index():
-    return redirect('/contacts')
+    return redirect('/posts')
 
-@app.route('/contacts')
+@app.route('/posts')
 def contacts():
     search = request.args.get('q')
     if search is not None:
-        contacts_set = Contact.search(search)
+        posts_set = Post.search(search)
     else:
-        contacts_set = Contact.all()
+        posts_set = Post.all()
 
-    return render_template('index.html', contacts=contacts_set)
+    return render_template('index.html', posts=posts_set)
+
+@app.route('/posts/new', methods=['GET'])
+def contacts_new_get():
+    return render_template('new.html', post=Post())
+
+@app.route('/posts/new', methods=['POST'])
+def contacts_new():
+    p = Post(
+        None,
+        request.form['title'],
+        request.form['body'],
+        str(datetime.date.today())
+    )
+    if p.save():
+        flash("Created New Post!")
+        return redirect('/posts')
+    else:
+        return render_template('new.html', post=p)
---
+++ posts_model.py
@@ -0,0 +1,71 @@
+import json
+
+PAGE_SIZE = 5
+
+class Post:
+    db = {}
+    
+    def __init__(self, id_=None, title=None, body=None, created=None):
+        self.id = id_
+        self.title = title
+        self.body = body
+        self.created = created
+        self.errors = {}
+
+    def __str__(self):
+        return json.dumps(self.__dict__, ensure_ascii=False)
+
+    def validate(self):
+        if not self.title:
+            self.errors['title'] = "Title Required"
+        if not self.body:
+            self.errors['body'] = "Body Required"
+        existing_post = next(filter(lambda p: p.id != self.id and p.title == self.title, Post.db.values()), None)
+        if existing_post:
+            self.errors['title'] = "Title Must Be Unique"
+        return len(self.errors) == 0
+
+    def save(self):
+        if not self.validate():
+            return False
+        if self.id is None:
+            if len(Post.db) == 0:
+                max_id == 1
+            else:
+                max_id = max(post.id for post in Post.db.values())
+            self.id = max_id + 1
+            Post.db[self.id] = self
+            Post.save_db()
+            return True
+
+    @classmethod
+    def all(cls, page=1):
+        page = int(page)
+        start = (page - 1) * PAGE_SIZE
+        end = start + PAGE_SIZE
+        return list(cls.db.values())[start:end]
+
+    @classmethod
+    def search(cls, text):
+        result = []
+        for p in cls.db.values():
+            match_title = p.title is not None and text in p.title
+            match_created = p.created is not None and text in p.created
+            if match_title or match_created:
+                result.append(p)
+
+        return result
+
+    @classmethod
+    def load_db(cls):
+        with open('posts.json', 'r') as posts_file:
+            posts = json.load(posts_file)
+            cls.db.clear()
+            for p in posts:
+                cls.db[p['id']] = Post(p['id'], p['title'], p['body'], p['created'])
+
+    @staticmethod
+    def save_db():
+        out_arr = [p.__dict__ for p in Post.db.values()]
+        with open("posts.json", "w") as f:
+            json.dump(out_arr, f, indent=2)
